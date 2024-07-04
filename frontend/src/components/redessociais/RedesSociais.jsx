ad1ccbb5df66df1ff59b23f7c653c6da91331f2d
Rey Hall <reyh4ll@gmail.com>
2024-07-04 15:18:12 -0300

    Advogados [Frontend/RedesSociais] - Adicionando feed do instagram com Graph Api

--- frontend/src/components/redessociais/RedesSociais.jsx
+++ frontend/src/components/redessociais/RedesSociais.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation} from 'swiper/modules';
 import { BiLeftArrow } from "react-icons/bi";
 import { BiRightArrow } from "react-icons/bi";
 import UnderlineSimbol from '../../assets/underline-simbol.svg';
 import './RedesSociais.css';
 
 function RedesSociais() {
+  const swiperRef = React.useRef(null);
+  const [isMouseOver, setIsMouseOver] = React.useState(false);
+  const [feedlist, setFeedList] = React.useState([]);
+  const token = import.meta.env.VITE_INSTA_TOKEN;
+  const fields = "media_url,media_type,permalink,timestamp";
+  const URL = `https://graph.instagram.com/me/media?access_token=${token}&fields=${fields}`;
+
+  async function getInstaFeed(){
+    const response = await fetch(URL);
+    const resolve = await response.json();
+    console.log(resolve.data)
+    setFeedList(resolve.data);
+  }
+
+  React.useEffect(() => {
+    getInstaFeed();
+  }, [])
+
+  const feedSort = feedlist.slice().sort((a,b) => new Date(b.timestamp) - new Date(a.timestamp));
+
   return (
     <section className="redes-sociais" id="redessociais">
       <div className="container">
@@ -14,6 +36,29 @@ function RedesSociais() {
           </h2>
           <h3>Nosso escritório nas redes sociais</h3>
         </hgroup>
+        <Swiper
+          ref={swiperRef}
+          navigation
+          breakpoints={{
+            0: { slidesPerView: 1, spaceBetween: 20},
+            640: { slidesPerView: 2, spaceBetween: 20},
+            768: { slidesPerView: 2, spaceBetween: 20},
+            1024: { slidesPerView: 3, spaceBetween: 20},
+          }}
+          modules={[Navigation]}
+        >
+          {feedSort.map(({id, media_url, media_type, permalink}) => (
+            <SwiperSlide className="slide" key={id}>
+              <a href={permalink} target="_blank" rel="noopener noreferrer">
+                {
+                  media_type === "IMAGE" || media_type === "CAROUSEL_ALBUM" ? 
+                  <img src={media_url}/> : 
+                  <video src={media_url}></video>
+                }
+              </a>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   )
